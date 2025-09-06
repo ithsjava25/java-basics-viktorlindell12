b737fb1cc9d800e921eef54f3a8759b6e3775a9c
Martin Blomberg <martin.blomberg@outlook.com>
2025-09-06 18:05:51 +0200

    Move some files

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.example.api.ElpriserAPI;
+
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
